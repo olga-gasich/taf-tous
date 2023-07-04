086b7e8fea69bc6be83c305b3b8a00e3614cf2d3
Olga Gasich <olagasich@gmail.com>
2023-07-04 18:34:48 +0300

    add login post api tests

--- src/test/java/api/tests/LoginTest.java
+++ src/test/java/api/tests/LoginTest.java
@@ -1,7 +1,110 @@
 package api.tests;
 
 import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import static io.restassured.RestAssured.given;
 
 public class LoginTest {
 
+    @Test
+    public void testLoginWithEmptyEmail(){
+
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("client_id","web_ey");
+        queryParams.put("client_secret","secret");
+        queryParams.put("grant_type","password");
+        queryParams.put("username","");
+        given().when().queryParams(queryParams).post("https://www.tous.com/authorizationserver/oauth/token")
+                .then().log().status().log().body();
+    }
+
+    @Test
+    public void testLoginWithEmptyPassword(){
+
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("client_id","web_ey");
+        queryParams.put("client_secret","secret");
+        queryParams.put("grant_type","password");
+        queryParams.put("password","");
+        given().when().queryParams(queryParams).post("https://www.tous.com/authorizationserver/oauth/token")
+                .then().log().status().log().body();
+    }
+
+    @Test
+    public void testLoginWithEmptyEmailAndPassword(){
+
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("client_id","web_ey");
+        queryParams.put("client_secret","secret");
+        queryParams.put("grant_type","password");
+        queryParams.put("username","");
+        queryParams.put("password","");
+        given().when().queryParams(queryParams).post("https://www.tous.com/authorizationserver/oauth/token")
+                .then().log().status().log().body();
+    }
+
+    @Test
+    public void testLoginWithEmptyEmailAndInvalidPassword(){
+
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("client_id","web_ey");
+        queryParams.put("client_secret","secret");
+        queryParams.put("grant_type","password");
+        queryParams.put("username","");
+        queryParams.put("password","12345");
+        given().when().queryParams(queryParams).post("https://www.tous.com/authorizationserver/oauth/token")
+                .then().log().status().log().body();
+    }
+
+    @Test
+    public void testLoginWithInvalidEmailAndEmptyPassword(){
+
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("client_id","web_ey");
+        queryParams.put("client_secret","secret");
+        queryParams.put("grant_type","password");
+        queryParams.put("username","o.test@gmail.com");
+        queryParams.put("password","");
+        given().when().queryParams(queryParams).post("https://www.tous.com/authorizationserver/oauth/token")
+                .then().log().status().log().body();
+    }
+
+    @Test
+    public void testLoginWithInvalidEmailAndValidPassword(){
+
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("client_id","web_ey");
+        queryParams.put("client_secret","secret");
+        queryParams.put("grant_type","password");
+        queryParams.put("username","o.test@gmail.com");
+        queryParams.put("password","Pwd1122334455");
+        given().when().queryParams(queryParams).post("https://www.tous.com/authorizationserver/oauth/token")
+                .then().log().status().log().body();
+    }
+
+    @Test
+    public void testLoginWithValidEmailAndEmptyPassword(){
+
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("client_id","web_ey");
+        queryParams.put("client_secret","secret");
+        queryParams.put("grant_type","password");
+        queryParams.put("username","olagasich@gmail.com");
+        queryParams.put("password","");
+        given().when().queryParams(queryParams).post("https://www.tous.com/authorizationserver/oauth/token")
+                .then().log().status().log().body();
+    }
+
+    @Test
+    public void testLoginWithValidCredentials(){
+
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("client_id","web_ey");
+        queryParams.put("client_secret","secret");
+        queryParams.put("grant_type","password");
+        queryParams.put("username","olagasich@gmail.com");
+        queryParams.put("password","Pwd1122334455");
+        given().when().queryParams(queryParams).post("https://www.tous.com/authorizationserver/oauth/token")
+                .then().log().status().log().body();
+    }
 }
